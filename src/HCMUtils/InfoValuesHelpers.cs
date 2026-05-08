55ebb55f892a621655eb385977504b1492ad8ba7
Phillip Kessels <Phillip.Kessels@BNetzA.de>
2026-05-08 10:31:06 +0200

    chore: add docs for info values helper

--- src/HCMUtils/InfoValuesHelpers.cs
+++ src/HCMUtils/InfoValuesHelpers.cs
@@ -4,6 +4,11 @@ using HCMUtils.Types;
 
 public class InfoValuesHelpers
 {
+    /// <summary>
+    /// Convert `InfoValues` back to an array of booleans.
+    /// </summary>
+    /// <param name="infoValues">The input info values to convert</param>
+    /// <returns>An array of booleans according to the input value as defined in the HCM agreement.</returns>
     public static bool[] ToInfoValuesArray(InfoValues infoValues) => [
             infoValues.TxSiteHeightFromDatabase,
             infoValues.TxSiteHeightDifferentFromDatabase,
