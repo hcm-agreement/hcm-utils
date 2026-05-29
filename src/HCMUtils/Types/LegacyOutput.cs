06eee8f8019a2c03a334dd33307c1137f7dd4a95
Phillip Kessels <Phillip.Kessels@BNetzA.de>
2026-05-29 10:17:57 +0200

    fix: calcuated sea temperature is never null

--- src/HCMUtils/Types/LegacyOutput.cs
+++ src/HCMUtils/Types/LegacyOutput.cs
@@ -2,7 +2,7 @@ namespace HCMUtils.Types;
 
 public record LegacyOutput(
     LegacyInput Input,
-    Temperature? SeaTemperature,
+    Temperature SeaTemperature,
     string VersionNumber,
     (double Long, double Lat) CalculatedTxCoordinates,
     (double Long, double Lat) CalculatedRxCoordinates,
